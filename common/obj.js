0d2dd1c9a7c4f1648a978b2f1eb57ac7fc9113b3
Alex Milanov <alex@i4web.biz>
2016-12-26 18:30:30 +0200

    added sub selector for object

--- common/obj.js
+++ common/obj.js
@@ -14,13 +14,24 @@ const patch = (o, k, v) => Object.assign({}, o,
 		: keyValue(k, typeof o[k] === 'object' && Object.assign({}, o[k], v) || v)
 );
 
+const sub = (o, p) => (p instanceof Array)
+	&& o[p[0]] && sub(o[p[0]], p.slice(1))
+	|| o[p] || false;
+
 // console.log(patch({}, ['a', 'b', 'c'], 'boom'));
 // console.log(patch({}, 'x', 1));
 // console.log(['a', 'b', 'c'].slice(1));
 //
 // console.log(patch({a: {d: '1'}}, 'a', {g: 2}));
 
+// let o = {
+// 	a: {b: {c: 123}}
+// };
+//
+// console.log(sub(o, ['a', 'b', 3]));
+
 module.exports = {
 	keyValue,
-	patch
+	patch,
+	sub
 };
